070af1420077ca12c8e90c88bfe95b02ef9490a0
Erik Vullings <erik.vullings@gmail.com>
2021-03-23 14:57:19 +0100

    Add support for folder-based DB files

--- src/serve.ts
+++ src/serve.ts
@@ -1,10 +1,13 @@
 import fs from 'fs';
+import path from 'path';
 import { config } from './config';
 import { createApi, db } from './index';
 import { ILokiConfiguration } from './models';
 import { ICommandOptions } from './models/command-options';
 
 export const startService = (configuration: ICommandOptions = config) => {
+  const folderPath = path.dirname(path.resolve(process.cwd(), configuration.db || ''));
+  if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath, { recursive: true });
   const dbOptions =
     configuration.config && fs.existsSync(configuration.config)
       ? (JSON.parse(fs.readFileSync(configuration.config).toString()) as ILokiConfiguration)
